333444594282bd86f2daf58b6673d548c2a5bc37
valentino17-0 <82229577+valentino17-0@users.noreply.github.com>
2022-11-12 09:28:47 -0500

    version 6

--- spring-01/src/main/java/com/certus/spring/controller/HomeController.java
+++ spring-01/src/main/java/com/certus/spring/controller/HomeController.java
@@ -1,5 +1,10 @@
 package com.certus.spring.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +17,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.certus.spring.models.Personaje;
 import com.certus.spring.models.Response;
@@ -22,28 +29,22 @@ import com.certus.spring.service.IPersonajeService;
 @Controller
 @RequestMapping("/app")
 @SessionAttributes("personaje")
-
 public class HomeController {
 
 	@Value("${title.generic}")
 	private String titlePage;
 	
 	@Value("${mensaje}")
-	private String mensaje;	
+	private String mensaje;
 		
 	@Autowired
 	@Qualifier("servicio1")
-	private IPersonajeService InterfacePersonaje1; 
-	
-	@Autowired
-	@Qualifier("servicio2")
-	private IPersonajeService InterfacePersonaje2; 
-	
+	private IPersonajeService InterfacePersonaje1;
 
 	@GetMapping({ "/home", "/inicio", "/", "/Home", "/Inicio" })
 	public String Home(Model model) {
 		model.addAttribute("TituloPagina", titlePage);
-		model.addAttribute("titulo", "Sección J98");	
+		model.addAttribute("titulo", "Sección J98");				
 		model.addAttribute("Mensaje", mensaje);
 		
 		return "Home";
@@ -54,33 +55,25 @@ public class HomeController {
 	public String ListarPersonajes(Model model) {
 		
 		model.addAttribute("TituloPagina", titlePage);
-		model.addAttribute("titulo", "Seccion J98");	
-		
+		model.addAttribute("titulo", "Sección J98");
 		Response<Personaje> rspta = InterfacePersonaje1.listarPersonaje();
 		
 		if (rspta.getEstado()) {
-			
 			model.addAttribute("Mensaje", rspta.getMensaje());
-			model.addAttribute("listita", rspta.getListData());
-			
+			model.addAttribute("listita", rspta.getListData());			
 			return "lista";
-			
 		}else {
 			model.addAttribute("mensaje", rspta.getMensaje());
 			model.addAttribute("mensajeError", rspta.getMensajeError());
 			return "errores";
-		}
-		
-
-		
+		}	
 	}
-		
+	
+	
 	
 	@GetMapping("/crear")
 	public String Formulario(Model model) {
 		Personaje personaje =  new Personaje();
-		
-		personaje.setNombres(personaje.toString());
 		
 		model.addAttribute("TituloPagina", titlePage);
 		model.addAttribute("titulo", "Sección J98 - Crear Personaje");		
@@ -89,51 +82,86 @@ public class HomeController {
 		return "Formulario";		
 	}
 	
-	
 	@GetMapping("/Editar/{idPersonaje}")
 	public String EditarPersonaje(@PathVariable int idPersonaje, Model model) {
 		
-		model.addAttribute("TituloPagina", titlePage);
+		
+		model.addAttribute("TituloPagina", titlePage);	
 		
 		Response<Personaje> rspta = InterfacePersonaje1.editarPersonaje(idPersonaje);
-		model.addAttribute("titulo", "Sección J98 - Editando el personaje" +rspta.getData().getNombres());
+		
+		model.addAttribute("titulo", "Sección J98 - Editando el personaje "+rspta.getData().getNombres());
+		
 		model.addAttribute("personaje", rspta.getData());
+				
 		
 		return "Formulario";
 	}
 	
-	@GetMapping("/Eliminar/{idPersonaje}")
-	public String EliminarPersonaje(@PathVariable int idPersonaje, Model model) {
+	@GetMapping("/Elimnar/{idPersonaje}")
+	public String ElimnarPersonaje(@PathVariable int idPersonaje, Model model) {
+		
 		
 		Response<Personaje> rspta = InterfacePersonaje1.eliminarPersonaje(idPersonaje);
 		
-		
-		return "redirect:/app/listar";
-	}
-	
-	
-	
-	@PostMapping("/form")
-	public String creaPersonaje(@Valid Personaje Luffy,
-			                    BindingResult result,
-			                     Model model,
-			                     SessionStatus Status) {
-		
-		if(result.hasErrors()) {
-			return "Formulario";
-		}
-				
-		Response<Personaje> rspta = InterfacePersonaje1.crearPersonaje(Luffy);
-		
-		if (rspta.getEstado()) {
-			return "redirect:/app/listar"; //redireccione a la lista
-			
+		if (rspta.getEstado()) {			
+			return "redirect:/app/listar";	
 		}else {
 			model.addAttribute("mensaje", rspta.getMensaje());
 			model.addAttribute("mensajeError", rspta.getMensajeError());
+			
 			return "errores";
-		
 		}
+	}
+	
+	@PostMapping("/form")
+	public String creaPersonaje(@Valid Personaje Luffy, 
+								BindingResult result, 
+								Model model,
+								@RequestParam("ImagenDelFormulario") MultipartFile fileRecibido,
+								SessionStatus sStatus) {
+		
+		if(result.hasErrors()) {						
+			return "Formulario";
+		}
+		
+		if (!fileRecibido.isEmpty()) {			
+			Path rutaRelativaFile = Paths.get("src//main//resources//UploadsImg");
+			String rutaAbsoluta = rutaRelativaFile.toFile().getAbsolutePath();
+			
+			try {
+				byte [] bytesFile = fileRecibido.getBytes();
+				Path enlaceAGuardar = Paths.get(rutaAbsoluta+"//"+fileRecibido.getOriginalFilename());
+				Files.write(enlaceAGuardar, bytesFile);
+				
+				Luffy.setUriImagen(fileRecibido.getOriginalFilename());
+				
+			} catch (IOException e) {
+				model.addAttribute("mensaje", "Error al procesar la Imagen");
+				model.addAttribute("mensajeError", e.getStackTrace());
+				return "errores";			
+			}
+			
+		}else {
+			model.addAttribute("Estado", "El personaje requiere una imagen");			
+			return "Formulario";
+		}		
+		
+		
+		Response<Personaje> rspta = InterfacePersonaje1.crearPersonaje(Luffy);
+		
+		
+		if (rspta.getEstado()) {
+			
+			sStatus.setComplete();
+			return "redirect:/app/listar";	
+			
+			}else {
+			model.addAttribute("mensaje", rspta.getMensaje());
+			model.addAttribute("mensajeError", rspta.getMensajeError());
+			return "errores";
+		}
+		
 	}
 	
 
